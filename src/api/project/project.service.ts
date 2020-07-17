8e967411b73babe4a05752ed3a0dc795defcca4d
Deby Silvia Agnes <deby.silvia23@gmail.com>
2020-07-17 21:02:54 +0700

    feat(project): implement endpoint edit project (#6)

--- src/api/project/project.service.ts
+++ src/api/project/project.service.ts
@@ -1,16 +1,39 @@
-import { ProjectRequest } from "./project.type";
-import projectModel from "./project.model";
+import { ProjectRequest } from './project.type'
+import projectModel, { ProjectDocument } from './project.model'
+import HttpException from '../../utils/httpException'
 
 export default class ProjectService {
   createProject(project: ProjectRequest) {
-    return projectModel.create({ ...project });
+    return projectModel.create({ ...project })
   }
 
   getAllProject() {
-    return projectModel.find({ "isDeleted": false });;
+    return projectModel.find({ isDeleted: false })
+  }
+
+  editProject(id: string, project: ProjectRequest) {
+    return new Promise<ProjectDocument>(async (resolve, reject) => {
+      try {
+        const editedProject = await projectModel.findByIdAndUpdate(
+          id,
+          project,
+          {
+            new: true,
+          }
+        )
+        if (editedProject) resolve(editedProject)
+        else throw new HttpException(400, 'project not found')
+      } catch (error) {
+        reject(error)
+      }
+    })
   }
 
   deleteProject(id: string) {
-    return projectModel.findByIdAndUpdate(id, { "isDeleted": true }, { new: true, lean: true });
+    return projectModel.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      { new: true, lean: true }
+    )
   }
 }
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,52 +1,44 @@
-require("dotenv").config();
-import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
-import projectModel from "../api/project/project.model";
+require('dotenv').config()
+import mongoose from 'mongoose'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import projectModel from '../api/project/project.model'
 
-const mongoMemoryServer = new MongoMemoryServer();
-const { DB_USER, DB_PASS, DB_NAME } = process.env;
+const mongoMemoryServer = new MongoMemoryServer()
+const { DB_USER, DB_PASS, DB_NAME } = process.env
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-};
+}
 
 async function getConnectionString(isUsingMemory?: boolean) {
   return isUsingMemory
     ? await mongoMemoryServer.getConnectionString()
-    : `mongodb+srv://${DB_USER}:${DB_PASS}@clusterblueprint.yvkef.gcp.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+    : `mongodb+srv://${DB_USER}:${DB_PASS}@clusterblueprint.yvkef.gcp.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
 }
 
 export async function connectDB(isUsingMemory?: boolean) {
-  const connectionString = await getConnectionString(isUsingMemory);
-  return mongoose.connect(connectionString, options);
+  const connectionString = await getConnectionString(isUsingMemory)
+  return mongoose.connect(connectionString, options)
 }
 
-// mongoose.connection.on("error", function (error) {
-//   console.error("Database connection error:", error);
-// });
-
-// mongoose.connection.once("open", function () {
-//   console.log("Database connected");
-// });
-
 export async function clearDB() {
-  return await mongoose.connection.db.dropDatabase();
+  return await mongoose.connection.db.dropDatabase()
 }
 
 export async function closeDB(isUsingMemory?: boolean) {
-  await mongoose.connection.dropDatabase();
-  await mongoose.connection.close();
-  if (isUsingMemory) await mongoMemoryServer.stop();
+  await mongoose.connection.dropDatabase()
+  await mongoose.connection.close()
+  if (isUsingMemory) await mongoMemoryServer.stop()
 }
 
 export async function mockingDatabaseRecord() {
   await projectModel.create({
-    name: "peta kandang singa 1",
-    description: "kandang singa ini berada disebelah kandang harimau 1",
-  });
+    name: 'peta kandang singa 1',
+    description: 'kandang singa ini berada disebelah kandang harimau 1',
+  })
   await projectModel.create({
-    name: "peta kandang singa 2",
-    description: "kandang singa ini berada disebelah kandang harimau 2",
-  });
+    name: 'peta kandang singa 2',
+    description: 'kandang singa ini berada disebelah kandang harimau 2',
+  })
 }
