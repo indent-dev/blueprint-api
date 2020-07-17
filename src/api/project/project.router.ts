4fea4f57400c7658e03b53a07513dc3953a87f84
Naufal Yudhistira <yudhistn@gmail.com>
2020-07-17 18:23:34 +0700

    feat(project): Implement sanitize HTML char (#5)
    * feat(project): Implement sanitize HTML char
    
    * feat(prettier): Add prettier config

--- src/api/project/project.router.ts
+++ src/api/project/project.router.ts
@@ -1,12 +1,22 @@
-import { Router } from "express";
-import ProjectController from "./project.controller";
+import { Router } from 'express'
+import { body } from 'express-validator'
+import ProjectController from './project.controller'
 
-const projectRouter = Router();
-const projectController = new ProjectController();
-const baseUrl = "/project";
+const projectRouter = Router()
+const projectController = new ProjectController()
+const baseUrl = '/project'
 
-projectRouter.post(`${baseUrl}`, projectController.store);
-projectRouter.get(`${baseUrl}`, projectController.index);
-projectRouter.delete(`${baseUrl}/:id`, projectController.delete);
+/*  change
+ *   <p>halo dunia</p>
+ *   into
+ *   p&gt;halo dunia&lt;&#x2F;p&gt;
+ */
+projectRouter.post(
+  `${baseUrl}`,
+  [body('name').escape(), body('description').escape()],
+  projectController.store
+)
+projectRouter.get(`${baseUrl}`, projectController.index)
+projectRouter.delete(`${baseUrl}/:id`, projectController.delete)
 
-export default projectRouter;
+export default projectRouter
