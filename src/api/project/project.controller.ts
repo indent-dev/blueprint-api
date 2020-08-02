b6dbaf6f322831a81e920ab4976dc062a40ec9db
SultanKs4 <sultangendut@gmail.com>
2020-08-02 18:55:16 +0700

    fix(project): rename function sanitizer, move validation handler to separate file

--- src/api/project/project.controller.ts
+++ src/api/project/project.controller.ts
@@ -1,28 +1,20 @@
 import { Request, Response, NextFunction } from 'express'
 import ProjectService from './project.service'
 import HttpException from '../../utils/httpException'
-import { validationResult } from 'express-validator'
 const projectService = new ProjectService()
 
 export default class ProjectController {
   async store(req: Request, res: Response, next: NextFunction) {
     try {
-      validationResult(req).throw()
       const project = await projectService.createProject(req.body)
       res.send(project)
     } catch (error) {
-      next(
-        new HttpException(
-          error.statusCode || 400,
-          error.message || error.mapped()
-        )
-      )
+      next(new HttpException(error.statusCode || 500, error.message))
     }
   }
 
   async index(req: Request, res: Response, next: NextFunction) {
     try {
-      validationResult(req).throw()
       const { page, itemPerPage, sortBy, sortDirection, name = '' } = req.query
       const result = await projectService.getAllProject(
         Number(page),
@@ -33,43 +25,26 @@ export default class ProjectController {
       )
       res.send(result)
     } catch (error) {
-      next(
-        new HttpException(
-          error.statusCode || 400,
-          error.message || error.mapped()
-        )
-      )
+      next(new HttpException(error.statusCode || 500, error.message))
     }
   }
 
   async edit(req: Request, res: Response, next: NextFunction) {
     try {
-      validationResult(req).throw()
       const { id } = req.params
       const project = await projectService.editProject(id, req.body)
       res.send(project)
     } catch (error) {
-      next(
-        new HttpException(
-          error.statusCode || 400,
-          error.message || error.mapped()
-        )
-      )
+      next(new HttpException(error.statusCode || 500, error.message))
     }
   }
 
   async delete(req: Request, res: Response, next: NextFunction) {
     try {
-      validationResult(req).throw()
       const result = await projectService.deleteProject(req.params.id)
       res.send(result)
     } catch (error) {
-      next(
-        new HttpException(
-          error.statusCode || 400,
-          error.message || error.mapped()
-        )
-      )
+      next(new HttpException(error.statusCode || 500, error.message))
     }
   }
 }
